6c0aeb7ac8d4940872f9ee6d19e634443a262bf5
Brandon Tran <brandon@Brandons-MacBook-Air.local>
2017-11-24 21:53:24 +0800

    yargs and require

--- app.js
+++ app.js
@@ -2,5 +2,26 @@ console.log('--> app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
-const notes = require('./notes.js');
+const note = require('./note.js');
+
+const argv = yargs.argv;
+
+var command = process.argv[2];
+
+console.log(argv);
+console.log(`Command: ${argv._[0]}`);
+
+if (command === 'list') {
+  note.getAll();
+} else if (command === 'read') {
+  note.getNote(argv.title);
+} else if (command === 'remove') {
+  note.removeNote(argv.title);
+} else if (command === 'add') {
+  note.addNote(argv.title, argv.body);
+} else {
+  console.log('Wrong command, dumb dawg.');
+
+}
--- note.js
+++ note.js
@@ -1,6 +1,24 @@
 console.log('--> note.js');
 
-module.exports.addNote = () => {
-  console.log('Adding note.');
-  return 'Add me on MySpace.'
+var addNote = (title, body) => {
+  console.log('Adding note with title:', title, 'and body:', body);
+};
+
+var getAll = () => {
+  console.log('Getting all nodes');
+};
+
+var removeNote = (title) => {
+  console.log('Removing note with title:', title);
+};
+
+var getNote = (title) => {
+  console.log('Reading note with title:', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  removeNote,
+  getNote
 };
